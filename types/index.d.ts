c3fd90fa1100df95e8976f5aac4cc0e685d1ddf3
Hauke Thorenz <hauke.thorenz@weidmueller.com>
2024-07-26 15:15:40 +0200

    use tcwdioappium:resizeStrategy

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,7 +1,22 @@
 import { RemoteOptions } from "webdriverio";
 import { SafariCapabilities } from "./safari";
+
+export type ResizeStrategies = "setWindowSize" | "rotateDevice" | "setOrientation";
+export type TcWdioAppiumOptions = {
+	/**
+	 * On mobile devices, there is no window to resize.
+	 * If width >= height, the window/device is in landscape mode, portait otherwise.
+	 *
+	 * * `"auto"` use best guesses based on the driver and device (default)
+	 * * `"setWindowSize"` will use https://webdriver.io/docs/api/browser/setWindowSize/
+	 * * `"rotateDevice"` will use https://webdriver.io/docs/api/appium/#rotatedevice
+	 * * `"setOrientation"` will use https://webdriver.io/docs/api/jsonwp/#setorientation
+	 */
+	"tcwdioappium:resizeStrategy"?: "auto" | ResizeStrategies,
+};
+
 type SafariRemoteOptions = RemoteOptions & {capabilities: SafariCapabilities};
 
 export type TcWdioAppiumConfig = {
-	[name: string]: RemoteOptions | SafariRemoteOptions,
+	[name: string]: TcWdioAppiumOptions & (RemoteOptions | SafariRemoteOptions),
 };
